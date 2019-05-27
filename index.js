37ab519f91a92abecf328c4d6051428ea07c35d9
Utsav Baral <manutsssav@gmail.com>
2019-05-27 11:18:17 +0545

    change made

--- index.js
+++ index.js
@@ -15,6 +15,26 @@ app.get('/api/courses/:id', (req, res) => {
    const course = courses.find(c => c.id === parseInt(req.params.id));
     if(!course) res.status(404).send('The book with the given ID not found');
     res.send(course);
-    const port = process.env.PORT || 3000;
-    app.listen(port, () => console.log(`listening to port ${port}`));
 });
+var isPrime = function (num){
+    if (num == 1 || num == 2){
+        return true
+    }
+    for (var i=2; i<num; i++){
+        if (num % i == 0){
+            return false
+        }
+    }
+    return true
+}
+app.post('/api/prime', (req, res) => {
+    var num = req.body.num
+    if (isPrime(num)){
+        res.send(num + 'is prime number')
+    }
+    else{
+        res.send(num+ 'is not prime')
+    }
+})
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening to port ${port}`));
